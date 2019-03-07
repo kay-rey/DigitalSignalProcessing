441818dd7eeee572b43f1d8c3e5e4d84b8d0f528
K-Rey <kevbreyes@gmail.com>
2019-03-06 16:06:04 -0800

    Created Lab3 folder and changed Lab2
    finsihed version of Lab2

--- Lab2/conv_rt.m
+++ Lab2/conv_rt.m
@@ -1,11 +1,10 @@
- function y = conv_rt(x,h)
-            % Convolve two finite-length arrays, x and h
-                    %          returning array, y
-                    h_hat = h(end:-1:1);
-                    x_hat = [zeros(1,length(h)-1) x zeros(1,length(h)-1)];
-                    y = zeros(1,length(x)+length(h)-1);
+function y = conv_rt(x,h)
+% Convolve two finite-length arrays, x and h returning array, y
+h_hat = h(end:-1:1);
+x_hat = [zeros(1,length(h)-1) x zeros(1,length(h)-1)];
+y = zeros(1,length(x)+length(h)-1);
 
-                    for i=1:length(x)+length(h)-1
-                        y(i) = sum(h_hat.*x_hat(i:i+length(h)-1));
-                    end
- end
+for i=1:length(x)+length(h)-1
+    y(i) = sum(h_hat.*x_hat(i:i+length(h)-1));
+end
+end
