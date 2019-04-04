cdc911a49bf01a5a80e21f1f2b17d25c1267466a
K-Rey <kevbreyes@gmail.com>
2019-04-03 17:49:19 -0700

    reindented everything and changed fplot to look better

--- Lab5/fplot.m
+++ Lab5/fplot.m
@@ -1,16 +1,16 @@
 function fplot(b,a)
-% FPLOT Z-transform frequency response. 
-% FPLOT(B,A,N) is the 256-point complex frequency response 
-% of the filter B/A: 
-% -1 -nb 
-% jw B(z) b(1) + b(2)z + .... + b(nb+1)z 
-% H(e) = ---- = --------------------------------- 
-% -1 -na 
-% A(z) a(1) + a(2)z + .... + a(na+1)z  
-% given numerator and denominator coefficients in vectors B and A.  
-% The frequency response is evaluated at 256 points equally spaced 
-% around the upper half of the unit circle. 
-% If A isn't specified, it defaults to [1]. 
+% FPLOT Z-transform frequency response.
+% FPLOT(B,A,N) is the 256-point complex frequency response
+% of the filter B/A:
+% -1 -nb
+% jw B(z) b(1) + b(2)z + .... + b(nb+1)z
+% H(e) = ---- = ---------------------------------
+% -1 -na
+% A(z) a(1) + a(2)z + .... + a(na+1)z
+% given numerator and denominator coefficients in vectors B and A.
+% The frequency response is evaluated at 256 points equally spaced
+% around the upper half of the unit circle.
+% If A isn't specified, it defaults to [1].
 
 B=fft(b,512);
 A=fft(a,512);
@@ -20,13 +20,13 @@ H = B./A;
 ax = 0:1/((length(H)/2)-1):1;
 subplot(2,1,1);
 ph = plot(ax,(abs(H(1:(length(H)/2)))));
-    grid on;
-    set(ph,'linewidth',2);
-    ylabel('|X( \omega )|');
-    xlabel('\omega (rad*\pi)');
-    XL = xlim;
-    xlim([0 XL(2)]);
-   
+grid on;
+set(ph,'linewidth',2);
+ylabel('|X( \omega )|');
+xlabel('\omega (rad*\pi)');
+XL = xlim;
+xlim([0 XL(2)]);
+
 
 subplot(2,1,2);
 imagH = imag(H(1:length(H)/2));
@@ -47,11 +47,11 @@ end
 phase2(103:114);
 
 ph2=plot(ax,phase2);
-    grid on;
-    set(ph2,'linewidth',2);
-    ylabel('\angle(X) ');
-    xlabel('\omega (rad*\pi)');
-    ylim([-1 1]);
+grid on;
+set(ph2,'linewidth',2);
+ylabel('\angle(X) ');
+xlabel('\omega (rad*\pi)');
+ylim([-1 1]);
 
 
 end
--- Lab5/zplot.m
+++ Lab5/zplot.m
@@ -1,15 +1,15 @@
 function zplot(b,a)
-% ZPLOT Plot a zero-pole plot. 
-% -1 -nb 
-% B(z) b(1) + b(2)z + .... + b(nb+1)z 
-% H(z) = ---- = --------------------------------- 
-% -1 -na 
-% A(z) a(1) + a(2)z + .... + a(na+1)z 
-% 
-% zplot(b, a) plots the zeros and poles which determined by vectors 
-% b and a 
-% The plot includes the unit circle and axes for reference, plotted 
-% in black. 
+% ZPLOT Plot a zero-pole plot.
+% -1 -nb
+% B(z) b(1) + b(2)z + .... + b(nb+1)z
+% H(z) = ---- = ---------------------------------
+% -1 -na
+% A(z) a(1) + a(2)z + .... + a(na+1)z
+%
+% zplot(b, a) plots the zeros and poles which determined by vectors
+% b and a
+% The plot includes the unit circle and axes for reference, plotted
+% in black.
 % Each zero is represented with a blue 'o' and each pole with a red
 % 'x' on the plot.
 
@@ -29,5 +29,27 @@ b_i= imag(zers);
 
 plot(a_r ,a_i, 'rx');
 plot(b_r,b_i, 'bo');
+
+if min(a_r) < min(b_r)
+	xmin = min(a_r);
+elseif min(b_r)< -1
+	xmin = min(b_r);
+else 
+	xmin = -1;
+end
+
+if min(a_i) < min(b_i)
+	ymin = min(a_i);
+elseif min(b_i)< -1
+	ymin = min(b_i);
+else 
+	ymin = -1;
+end
+
+axis equal;
+xlim([xmin*1.1 xmin*-1.1]);
+ylim([ymin*1.1 ymin*-1.1]);
+
+
 
 end
